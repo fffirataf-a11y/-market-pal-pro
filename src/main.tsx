4ac342a4e6c216a5e2cf5bc0effad1eea8826c61
ffiratai <firatfurkan@yahoo.com>
2025-12-28 21:19:15 +0300

    Debug v17: Add error tracing & fix z-index

--- src/main.tsx
+++ src/main.tsx
@@ -14,22 +14,29 @@ import './i18n'
 
 const queryClient = new QueryClient()
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HashRouter>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <AuthProvider>
-            <TooltipProvider>
-              <SubscriptionProvider>
-                <Toaster />
-                <Sonner />
-                <App />
-              </SubscriptionProvider>
-            </TooltipProvider>
-          </AuthProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </HashRouter>
-  </StrictMode>,
-)
+console.log('[DEBUG] main.tsx: Starting React render...')
+
+try {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <HashRouter>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>
+            <AuthProvider>
+              <TooltipProvider>
+                <SubscriptionProvider>
+                  <Toaster />
+                  <Sonner />
+                  <App />
+                </SubscriptionProvider>
+              </TooltipProvider>
+            </AuthProvider>
+          </ThemeProvider>
+        </QueryClientProvider>
+      </HashRouter>
+    </StrictMode>,
+  )
+} catch (error) {
+  console.error('[DEBUG] main.tsx: FATAL ERROR during render:', error);
+  document.body.innerHTML = '<h1 style="color:red;padding:20px">App Crash: ' + error + '</h1>';
+}
