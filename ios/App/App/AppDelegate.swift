a4fba4b1795ff02fcf585aa861b8eb736a3fd123
fffirataf-a11y <ff.firat.af@gmail.com>
2026-01-24 21:45:04 +0300

    fix: ios critical config and entitlements

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Capacitor
-// import FirebaseCore
-// import FirebaseMessaging
+import FirebaseCore
+import FirebaseMessaging
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -10,7 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        // FirebaseApp.configure()
+        FirebaseApp.configure()
         return true
     }
 
